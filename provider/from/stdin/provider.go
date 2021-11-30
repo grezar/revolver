17b4abab506a26d82a9c661d1a1372f466d16739
grezar <grezar.dev@gmail.com>
2021-11-30 21:11:20 +0900

    Drop support for input from a terminal

--- provider/from/stdin/provider.go
+++ provider/from/stdin/provider.go
@@ -2,7 +2,7 @@ package stdin
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"io"
 	"os"
 
@@ -46,8 +46,7 @@ type Spec struct{}
 func (s *Spec) Do(ctx context.Context) (secrets.Secrets, error) {
 	var input string
 	if isatty.IsTerminal(os.Stdin.Fd()) {
-		fmt.Printf("Input for the Stdin provider: ")
-		fmt.Scan(&input)
+		return nil, errors.New("Stdin provider does not support input from a terminal")
 	} else {
 		bytes, err := io.ReadAll(os.Stdin)
 		if err != nil {
